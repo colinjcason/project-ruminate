85ec16aaa127c5c22deeda99a1d2f4b4631b96c1
mdsimpson09 <131899003+mdsimpson09@users.noreply.github.com>
2025-01-11 16:41:59 -0800

    2 square, one large one small, 2 rectangle, one large, one small

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@
 import Image from "next/image";
 import RectangleButton from "../components/buttons/rectangle-button";
 import SmallRectangleButton from "../components/buttons/small-rectangle-button";
+import SmallSquareButton from "../components/buttons/small-square-button";
+import LargeSquareButton from "../components/buttons/square-button";
 
 export default function HomePage() {
   return (
@@ -11,11 +13,23 @@ export default function HomePage() {
         label="Button" 
         onClick={() => alert('Button Clicked!')} 
       /> */}
+      <br/>
       <SmallRectangleButton 
         label="Button" 
-        
       />
-      
+      <br/>
+       <SmallSquareButton className="mt-4" onClick={() => alert('Heart Clicked!')} />
+       <br/>
+       <RectangleButton 
+        label="Button" 
+        onClick={() => alert('Button Clicked!')} 
+      /> 
+       <br/>
+       {/* <SmallSquareButton onClick={() => alert('Heart Clicked!')} /> */}
+        <br/>
+        <LargeSquareButton 
+        onClick={() => alert('Button Clicked!')} 
+      /> 
     </main>
   );
 }
