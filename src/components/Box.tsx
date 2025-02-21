9153737f37a0af247a0c075163966999fc648df8
rjfuhrman42 <reidjf77@gmail.com>
2025-02-20 23:58:29 -0500

    feat: box component
    show box on main
    
    remove import
    
    whoops

---
+++ src/components/Box.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+type Props = {
+  title: string;
+  body: string;
+};
+
+function Box({ title, body }: Props) {
+  return (
+    <div className="bg-white p-4 text-base-0 rounded-lg shadow-[-6px_6px_0px_0px_#000] border-2 border-black">
+      <h3>{title}</h3>
+      <p>{body}</p>
+    </div>
+  );
+}
+
+export default Box;
