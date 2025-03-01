e19b77e5e904ea8e877886107257b1b485a1c969
rjfuhrman42 <reidjf77@gmail.com>
2025-02-28 20:20:30 -0500

    setup basic box style

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -7,9 +7,9 @@ type Props = {
 
 function Box({ title, body }: Props) {
   return (
-    <div className="semantic-secondary-100 p-4 text-base-0 rounded-lg shadow-[-6px_6px_0px_0px_#000] border-2 border-black">
+    <div className="flex flex-col gap-5 semantic-secondary-100 w-[19.25rem] py-14 px-8 text-base-0 rounded-[1rem] shadow-[-6px_6px_0px_0px_#000] border-2 border-black">
       <h3>{title}</h3>
-      <p>{body}</p>
+      <p className="font-[500]">{body}</p>
     </div>
   );
 }
