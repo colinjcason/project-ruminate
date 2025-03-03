d06d440593c396427de4182e1181725794b694bf
rjfuhrman42 <reidjf77@gmail.com>
2025-03-03 16:45:01 -0500

    fix: switch svgs to pngs, trim images

--- src/app/producer-landing-page/page.tsx
+++ src/app/producer-landing-page/page.tsx
@@ -1,11 +1,14 @@
 import Box from "@/components/Box";
-import { ChecklistIcon, LightbulbIcon, MedalIcon } from "@/components/icons";
+import Lightbulb from "../../assets/lightbulb.png";
+import Medal from "../../assets/medal.png";
+import Checklist from "../../assets/checklist.png";
+import Image from "next/image";
 
 export default function ProducerLandingPage() {
   return (
     <div className="bg-white">
       <main className="flex flex-col">
-        <section className="px-24 py-16 flex flex-col bg-semantic-accent-100 gap-y-20">
+        <section className="px-12 sm:px-24 py-16 flex flex-col bg-semantic-accent-100 gap-y-20">
           <div className="flex flex-col gap-y-5">
             <h2 className="big-header-normal">
               EXPERIENCED PROFESSIONALS <br></br>EXCEPTIONAL{" "}
@@ -18,21 +21,42 @@ export default function ProducerLandingPage() {
               understanding of customer preferences and buying behavior
             </p>
           </div>
-          <div className="flex justify-between gap-x-8">
+          <div className="flex flex-col flex-wrap items-center md:items-start gap-y-24 md:flex-row justify-between md:gap-x-8">
             <Box
               title="Carefully Vetted"
               body="Evaluators need at least four years of industry experience, a professional certification, or a supervisor recommendation."
-              cornerImage={<ChecklistIcon />}
+              cornerImage={
+                <Image
+                  src={Checklist}
+                  alt="a piece of paper with lines and check marks representing a check list"
+                  height={100}
+                  width={100}
+                />
+              }
             />
             <Box
               title="High Standards"
               body="We demand punctuality, clear communication, and thorough critiques from all evaluators."
-              cornerImage={<MedalIcon height={175} />}
+              cornerImage={
+                <Image
+                  src={Medal}
+                  alt="a medal icon with a two ribbons"
+                  height={110}
+                  width={110}
+                />
+              }
             />
             <Box
               title="Carefully Vetted"
               body="Evaluators need at least four years of industry experience, a professional certification, or a supervisor recommendation."
-              cornerImage={<LightbulbIcon />}
+              cornerImage={
+                <Image
+                  src={Lightbulb}
+                  alt="a lightlbulb icon with a lightning bolt in the center of the bulb"
+                  height={100}
+                  width={100}
+                />
+              }
             />
           </div>
         </section>
--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 function Box({ title, body, cornerImage }: Props) {
   return (
-    <div className="relative flex flex-col gap-5 bg-semantic-secondary-100 max-w-xs min-h-96 py-14 px-8 text-base-0 rounded-[1rem] shadow-[-6px_6px_0px_0px_#000] border-2 border-black">
+    <div className="flex-1 relative flex flex-col gap-5 bg-semantic-secondary-100 max-w-xs min-h-96 pt-8 px-8 text-base-0 rounded-[1rem] shadow-[-6px_6px_0px_0px_#000] border-2 border-black">
       <div className="absolute -top-16 -right-10">{cornerImage}</div>
       <h3>{title}</h3>
       <p className="font-[500]">{body}</p>
