f083f1c1e150ea0ee068804512a4059e69133a1a
rjfuhrman42 <reidjf77@gmail.com>
2025-03-09 17:56:07 -0400

    add border to button

--- src/app/producer-landing-page/page.tsx
+++ src/app/producer-landing-page/page.tsx
@@ -230,9 +230,9 @@ export default function ProducerLandingPage() {
               </PricingPlanBox>
             </div>
 
-            <button className="w-full self-center btn btn-primary sm:w-[21rem] bg-semantic-accent-900 text-semantic-accent-100 text-xl">
+            <button className="border-2 border-black w-full self-center btn btn-primary sm:w-[21rem] bg-semantic-accent-900 text-semantic-accent-100 text-xl shadow-[1px_2px_0px_0px_#000,_1px_4px_0px_0px_#000]">
               Get Started
-              <ArrowRightIcon />
+              <ArrowRightIcon height={15} width={15} fill="#e0d9e1" />
             </button>
           </div>
         </section>
